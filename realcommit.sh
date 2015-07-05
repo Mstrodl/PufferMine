2eed1c29bd038517b2460b6267ac4105c3744606
Mstrodl <Ipadlover8322@gmail.com>
2015-07-05 17:02:03 -0400

    Delete realcommit.sh

--- realcommit.sh
+++
@@ -1,8 +0,0 @@
-if [[ $(git log -1 | grep "#ArtifactUpload") != "" ]]; then
-    echo 'Nope! Just build artifacts (Awww...)'
-    exit 1
-fi
-if [[ $(git log -1 | grep "#ArtifactUpload") == "" ]]; then
-    echo "Yup! This is a real commit!"
-    exit 0
-fi
